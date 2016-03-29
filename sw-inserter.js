4091117829092e69ddee1f4cfe5ad3b8aa6a78cd
jmedley@google.com <jmedley@google.com>
2016-03-29 08:38:51 -0700

    Experiment with jsdom module.

--- sw-inserter.js
+++ sw-inserter.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var Transform = require('stream').Transform;
 var inherits = require('util').inherits;
+var jsdom = require('jsdom');
+var Transform = require('stream').Transform;
 
 module.exports = Insert;
 
@@ -17,26 +18,27 @@ function Insert(options) {
 inherits(Insert, Transform);
 
 Insert.prototype._transform = function transform(data, encoding, cb) {
-	//console.log('transform before : ' + JSON.stringify(data));
-	//console.log('transform before : ' + data.toString('UTF-8'));
 	// var str = data.toString('UTF-8');
-	// console.log('transform before : ' + str);
-	// var newBuff = new Buffer(str);
-	// console.log('transform before : ' + JSON.stringify(str));
+	// var addition = "<script src='site-assets/sw-loader.js'></script>";
+	// var locRegEx = '\s*<\/body>';
+	// var find = str.search(locRegEx);
+	// console.log(find);
+	// console.log(str.charAt(find));
+	// var res = str.replace(locRegEx, (addition + '\n</body>'));
+	// console.log(res);
 
-	// if (typeof data.originalValue === 'undefined') 
-	// 	data.originalValue = data.value;
-	// data.value++;
-	var str = data.toString('UTF-8');
-	var addition = "<script src='site-assets/sw-loader.js'></script>";
-	var locRegEx = '\s*<\/body>';
-	var find = str.search(locRegEx);
-	console.log(find);
-	console.log(str.charAt(find));
-	var res = str.replace(locRegEx, (addition + '\n</body>'));
-	console.log(res);
-	//console.log('transform after  : ' + JSON.stringify(data));
-	this.push(res);
+	var htmlFile = data.toString('UTF-8');
+	console.log(htmlFile);
+	jsdom.env(
+		htmlFile,
+		['site-assets/sw-loader.js'],
+		function (err, window) {
+			console.log("err: ", err);
+			//console.log("window: ", window);
+		}
+	);
+
+	this.push(htmlFile);
 	cb();
 }
 
